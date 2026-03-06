db906878fdaa7b4f93cf5474db8949d1ffcb0118
glazowr <harrydresden576@gmail.com>
2026-03-07 01:47:55 +0530

    Trivial Changes 1

--- discovery-service/src/main/java/com/vibe/vibediscovery/InstaDiscoveryApplication.java
+++ discovery-service/src/main/java/com/vibe/vibediscovery/InstaDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.vibe.instadiscovery;
+package com.vibe.vibediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class InstaDiscoveryApplication {
+public class VibeDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InstaDiscoveryApplication.class, args);
+		SpringApplication.run(VibeDiscoveryApplication.class, args);
 	}
 
 }
